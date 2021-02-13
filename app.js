0baab9d1b315775f79bd8209550bdd5fa9d3b6c6
Bisrat Fekede <it.bisrat.fekede@gmail.com>
2021-02-13 11:26:25 +0300

    Deleted back-end code and changed it to front-end javascript

--- app.js
+++ app.js
@@ -1,37 +1,26 @@
-var express = require('express')
-var fetch = require('node-fetch')
-var dotenv = require('dotenv')
-dotenv.config()
+const key = '87edfe8fa9d769d1fdc98d83269a9b9b';
 
-var app = express()
-var port = process.env.PORT || 6969
+(async function(){
+    let city = prompt('Enter a city')
+    // var city = 'addis ababa'
+    // const search_el = document.getElementById('search_el').value
+    const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
+    const query = `?q=${city}&appid=${key}&units=metric`;
 
+    //make fetch call (promise call)
+    const response = await fetch(baseURL + query);
 
-app.get('/', (req, res) => {
+    //promise data
+    const data = await response.json();
+    if (data.cod == '404') {
+        console.log('err')
+    }
 
-    (async function(){
-        var city = 'addis ababa'
-        // const search_el = document.getElementById('search_el').value
-        const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
-        const query = `?q=${city}&appid=${process.env.apiKey}&units=metric`;
+    // console.log(
+    //     `City : ${data.name} \n\n` + 'Coordinates : ' + `${data.coord.lon} Longitude` + ` ${data.coord.lat} Latitude` + '\n\n' + 'Weather Info : ' + `${data.weather[0].main}` + '\n\n' + `Temperature: ${data.main.temp}°C` + '\n\n'
+    // );
 
-        //make fetch call (promise call)
-        const response = await fetch(baseURL + query);
-
-        //promise data
-        const data = await response.json();
-        if (data.cod == '404') {
-            console.log('err')
-        }
-        res.json(data)
-
-        console.log(
-            'Coordinates : ' + `${data.coord.lon} Longitude` + ` ${data.coord.lat} Latitude` + '\n\n' + 'Weather Info : ' + `${data.weather[0].main}` + '\n\n' + `Temperature: ${data.main.temp}°C` + '\n\n' + `Min Temp: ${data.main.temp_min}°C` + '\n\n' + `Max Temp: ${data.main.temp_max}°C`
-        );
-    })()
-
-})
-
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+    alert(
+        `City : ${data.name} \n\n` + 'Coordinates : ' + `${data.coord.lon} Longitude` + ` ${data.coord.lat} Latitude` + '\n\n' + 'Weather Info : ' + `${data.weather[0].main}` + '\n\n' + `Temperature: ${data.main.temp}°C` + '\n\n'
+    )
+})()
