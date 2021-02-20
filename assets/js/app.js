a2fdc13d95db096f1dd525089c0720a391a02a59
Bisrat Fekede <it.bisrat.fekede@gmail.com>
2021-02-21 02:36:05 +0300

    added images to set background image transition

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,15 @@
 const key = '87edfe8fa9d769d1fdc98d83269a9b9b';
+const city = document.getElementById('searchInput')
+const searchButton = document.getElementById('searchButton')
 
-(async function(){
-    let city = prompt('Enter a city')
+searchButton.addEventListener('click', getWeather)
+
+async function getWeather(){
+    // let city = prompt('Enter a city')
     // var city = 'addis ababa'
     // const search_el = document.getElementById('search_el').value
     const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
-    const query = `?q=${city}&appid=${key}&units=metric`;
+    const query = `?q=${city.value}&appid=${key}&units=metric`;
 
     //make fetch call (promise call)
     const response = await fetch(baseURL + query);
@@ -23,4 +27,4 @@ const key = '87edfe8fa9d769d1fdc98d83269a9b9b';
     alert(
         `City : ${data.name} \n\n` + 'Coordinates : ' + `${data.coord.lon} Longitude` + ` ${data.coord.lat} Latitude` + '\n\n' + 'Weather Info : ' + `${data.weather[0].main}` + '\n\n' + `Temperature: ${data.main.temp}°C` + '\n\n'
     )
-})()
+}
