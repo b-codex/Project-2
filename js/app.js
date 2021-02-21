17d5cebed47a832360db82b4d2da1e9a76487e7f
Bisrat Fekede <it.bisrat.fekede@gmail.com>
2021-02-21 13:39:42 +0300

    added search bar on landing page

--- js/app.js
+++ js/app.js
@@ -7,12 +7,12 @@ new fullpage("#fullpage", {
   onLeave: (origin, destination, direction) => {
     const section = destination.item;
     const title = section.querySelector("h1");
-    const tl = new TimelineMax({ delay: 0.5 });
+    const tl = new TimelineMax({ delay: 0.1 });
     tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
     if (destination.index === 1) {
       const chairs = document.querySelectorAll(".chair");
       const description = document.querySelector(".description");
-      tl.fromTo(chairs, 0.7, { x: "100%" }, { x: "-10%" })
+      tl.fromTo(chairs, 0.1, { x: "100%" }, { x: "-10%" })
         .fromTo(
           description,
           0.5,
@@ -29,57 +29,35 @@ new fullpage("#fullpage", {
 
 // the api
 
-const key = '87edfe8fa9d769d1fdc98d83269a9b9b';
+// const key = '87edfe8fa9d769d1fdc98d83269a9b9b';
 
-(async function(){
-    let city = prompt('Enter a city')
-    // var city = 'addis ababa'
-    // const search_el = document.getElementById('search_el').value
-    const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
-    const query = `?q=${city}&appid=${key}&units=metric`;
+// (async function(){
+//     let city = 'london'
 
-    //make fetch call (promise call)
-    const response = await fetch(baseURL + query);
+//     const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
+//     const query = `?q=${city}&appid=${key}&units=metric`;
 
-    //promise data
-    const data = await response.json();
-    if (data.cod == '404') {
-        console.log('err')
-    }
+//     //make fetch call (promise call)
+//     const response = await fetch(baseURL + query);
 
-    // console.log(
-    //     `City : ${data.name} \n\n` + 'Coordinates : ' + `${data.coord.lon} Longitude` + ` ${data.coord.lat} Latitude` + '\n\n' + 'Weather Info : ' + `${data.weather[0].main}` + '\n\n' + `Temperature: ${data.main.temp}°C` + '\n\n'
-    // );
+//     //promise data
+//     const data = await response.json();
+//     if (data.cod == '404') {
+//         console.log('err')
+//     }
 
+//     let City = document.getElementById('City') 
+//     let Longitude = document.getElementById('Longitude') 
+//     let Latitude = document.getElementById('Latitude') 
+//     let Weather = document.getElementById('Weather') 
+//     let Temperature = document.getElementById('Temperature') 
 
-    // alert(
-    //     `City : ${data.name} \n\n` + 'Coordinates : ' + `${data.coord.lon} Longitude` + ` ${data.coord.lat} Latitude` + '\n\n' + 'Weather Info : ' + `${data.weather[0].main}` + '\n\n' + `Temperature: ${data.main.temp}°C` + '\n\n'
-
-    // )
-    let City = document.getElementById('City') 
-    let Longitude = document.getElementById('Longitude') 
-    let Latitude = document.getElementById('Latitude') 
-    let Weather = document.getElementById('Weather') 
-    let Temperature = document.getElementById('Temperature') 
-
-    City.innerHTML = `City : ${data.name}`;
-    Longitude.innerHTML = `Longitude: ${data.coord.lon}`;
-    Latitude.innerHTML = `Latitude: ${data.coord.lat}`;
-    Weather.innerHTML = `Current Weather forecast: ${data.weather[0].main}`
-    Temperature.innerHTML = `City : ${data.main.temp}°C` 
-    
-    //  `${data.coord.lon} Longitude` + ` ${data.coord.lat} Latitude` + '\n\n' + 'Weather Info : ' + `${data.weather[0].main}` + '\n\n' + `Temperature: ${data.main.temp}°C` + '\n\n'
-
-
-
-    // }else if (`${data.weather[0].main}` == "Rain"){
-    //     alert("Hooray!")
-    // }else if (`${data.weather[0].main}` == "Rain"){
-    //     alert("Hooray!")
-    // }else if (`${data.weather[0].main}` == "Rain"){
-    //     alert("Hooray!")
-    // }
-})()
+//     City.innerHTML = `City : ${data.name}`;
+//     Longitude.innerHTML = `Longitude: ${data.coord.lon}`;
+//     Latitude.innerHTML = `Latitude: ${data.coord.lat}`;
+//     Weather.innerHTML = `Current Weather forecast: ${data.weather[0].main}`
+//     Temperature.innerHTML = `City : ${data.main.temp}°C` 
+// })()
 
 var geoLocation = document.getElementById("geolocation-heading-one");
 function getLocation() {
---
+++ js/search.js
@@ -0,0 +1,66 @@
+let log = console.log
+
+const search = document.querySelector('#search')
+let City = document.getElementById('City')
+let Longitude = document.getElementById('Longitude')
+let Latitude = document.getElementById('Latitude')
+let Weather = document.getElementById('Weather')
+let Temperature = document.getElementById('Temperature')
+
+
+
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+    top: 2500,
+    left: 0,
+    behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position 
+window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+});
+
+search.addEventListener('keyup', (e) => {
+    const x = () => {
+        document.querySelector("#section-1 > div > h1").focus()
+        log('1')
+    }
+    if (e.keyCode === 13) {
+        e.preventDefault()
+
+        document.querySelector('#section-2').scrollIntoView({
+            behavior: 'smooth'
+        });
+
+        let city = e.target.value
+        const key = '87edfe8fa9d769d1fdc98d83269a9b9b';
+
+        (async function () {
+            // var city = 'addis ababa'
+            // const search_el = document.getElementById('search_el').value
+            const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
+            const query = `?q=${city}&appid=${key}&units=metric`;
+
+            //make fetch call (promise call)
+            const response = await fetch(baseURL + query);
+
+            //promise data
+            const data = await response.json();
+            if (data.cod == '404') {
+                console.log('err')
+            }
+            log(data)
+            City.innerHTML = `City : ${data.name}`;
+            Longitude.innerHTML = `Longitude: ${data.coord.lon}`;
+            Latitude.innerHTML = `Latitude: ${data.coord.lat}`;
+            Weather.innerHTML = `Current Weather forecast: ${data.weather[0].main}`
+            Temperature.innerHTML = `City : ${data.main.temp}°C`
+
+        })()
+
+    }
+})
